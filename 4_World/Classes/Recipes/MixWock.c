32b64cf581c52e1f5209a9e3f86e1ca9546aaa3b
chasir01 <kreigandmaya@gmail.com>
2024-08-21 21:27:23 -0400

    Init
    Initial Commit to the Wock Mod

---
+++ 4_World/Classes/Recipes/MixWock.c
@@ -0,0 +1,62 @@
+modded class PluginRecipesManager extends PluginBase
+{
+	 void RegisterRecipies()
+	{
+		super.RegisterRecipies();
+		RegisterRecipe(new ST_AgaricusLean);
+	};	
+};
+class ST_AgaricusLean extends RecipeBase
+{	
+	override void Init()
+	{
+		m_Name = "Mix up Wock :)"; //text that appears when you hover over an item (example: "create something")
+		m_IsInstaRecipe = false;//should this recipe be performed instantly without animation
+		m_AnimationLength = 1.00;//animation length in relative time units
+		m_Specialty = -0.02;// value > 0 for roughness, value < 0 for precision
+		
+		
+		//conditions
+		m_MinDamageIngredient[0] = -1;//-1 = disable check
+		m_MaxDamageIngredient[0] = 3;//-1 = disable check		
+		m_MinQuantityIngredient[0] = 1;//-1 = disable check
+		m_MaxQuantityIngredient[0] = -1;//-1 = disable check	
+		m_MinDamageIngredient[1] = -1;//-1 = disable check
+		m_MaxDamageIngredient[1] = 3;//-1 = disable check	
+		m_MinQuantityIngredient[1] = -1;//-1 = disable check
+		m_MaxQuantityIngredient[1] = -1;//-1 = disable check
+		
+		//INGREDIENTS
+		//ingredient 1
+		InsertIngredient(0,"AgaricusMushroom");//you can insert multiple ingredients this way
+		m_IngredientAddHealth[0] = 0;// 0 = do nothing
+		m_IngredientSetHealth[0] = -1; // -1 = do nothing
+		m_IngredientAddQuantity[0] = 0;// 0 = do nothing
+		m_IngredientDestroy[0] = true;//true = destroy, false = do nothing
+		m_IngredientUseSoftSkills[0] = false;// set 'true' to allow modification of the values by softskills on this ingredient	
+		
+		//ingredient 2		
+		InsertIngredient(1,"Bottle_Base");//you can insert multiple ingredients this way
+		m_IngredientAddHealth[1] = 0;// 0 = do nothing
+		m_IngredientSetHealth[1] = -1; // -1 = do nothing
+		m_IngredientAddQuantity[1] = 0;// 0 = do nothing
+		m_IngredientDestroy[1] = false ;// false = do nothing
+		m_IngredientUseSoftSkills[1] = false;// set 'true' to allow modification of the values by softskills on this ingredient	
+	
+		//result
+        AddResult("Lean_Agaricus");
+		
+		m_ResultSetFullQuantity[1] = true;//true = set full quantity, false = do nothing
+		m_ResultSetQuantity[0] = 500;//-1 = do nothing
+		m_ResultSetHealth[0] = -1;//-1 = do nothing
+		m_ResultInheritsHealth[0] = -1;// (value) == -1 means do nothing; a (value) >= 0 means this result will inherit health from ingredient number (value);(value) == -2 means this result will inherit health from all ingredients averaged(result_health = combined_health_of_ingredients / number_of_ingredients)
+		m_ResultInheritsColor[0] = -1;// (value) == -1 means do nothing; a (value) >= 0 means this result classname will be a composite of the name provided in AddResult method and config value "color" of ingredient (value)
+		m_ResultToInventory[1] = 0;//(value) == -2 spawn result on the ground;(value) == -1 place anywhere in the players inventory, (value) >= 0 means switch position with ingredient number(value)
+		m_ResultUseSoftSkills[0] = false;// set 'true' to allow modification of the values by softskills on this result
+		m_ResultReplacesIngredient[0] = 0;// value == -1 means do nothing; a value >= 0 means this result will transfer item propertiesvariables, attachments etc.. from an ingredient value
+	}
+
+
+	override bool CanDo(ItemBase ingredients[], PlayerBase player) { return true; /* final check for recipe's validity */}
+	override void Do(ItemBase ingredients[], PlayerBase player,array<ItemBase> results, float specialty_weight) { /* gets called upon recipe's completion */ }
+};	
---
+++ config.cpp
@@ -0,0 +1,93 @@
+class CfgPatches
+{
+    class Lean
+    {
+        units[] = {"Lean"};
+        requiredVersion = 0.1;
+        requiredAddons[] = {};
+    };
+};
+
+class CfgMods
+{
+	class Lean
+	{
+		dir = "leanskyleansky";
+		picture = "";
+		action = "";
+		hideName = 1;
+		hidePicture = 1;
+		name = "leanskyleansky";
+		credits = "Chasir01";
+		author = "Chasir01";
+		authorID = "0";
+		version = "1.0";
+		extra = 0;
+		type = "mod";
+        class defs
+        {
+            class worldScriptModule
+            {
+                files[] = {"leanskyleansky\4_world"};
+            };
+        };
+	};
+};
+
+class CfgVehicles
+{
+	class Bottle_Base;
+    class Lean_CupBase: Bottle_Base
+    {
+        scope = 2;
+        displayName = "LeanBase";
+        descriptionShort = "A 'cup' of Lean. You should not have this.";
+        model = "\leanskyleansky\assets\stteacan.p3d";
+        weight = 500;
+        itemSize[] = {1,2};
+        itemsCargoSize[] = {0,0};
+        inventorySlot[] = {"CookingEquipment","DirectCookingA","DirectCookingB","DirectCookingC"};
+        stackedRandom = 0;
+        canBeDigged = 1;
+        allowOwnedCargoManipulation = 1;
+        varLiquidTypeInit = 512;
+        liquidContainerType = "1 + 2 + 4 + 8 + 16 + 32 + 64 + 128 + 256 + 512 + 1024 + 2048 + 4096 + 8192 + 16384 + 32768 + 65536 + 131072 - (1 + 2 + 4 + 8 + 16 + 32 + 64 + 128 + 256) -32768";
+        destroyOnEmpty = 1;
+        varQuantityDestroyOnMin = 1;
+        varQuantityInit = 500;
+        varQuantityMin = 0;
+        varQuantityImax = 500;
+    };
+    class Lean_Agaricus: Lean_CupBase
+    {
+        displayName = "Lean";
+        descriptionShort = "A 'cup' of Lean.";
+        model = "\leanskyleansky\assets\stteacan.p3d";
+        itemSize[] = {2,1};
+        itemsCargoSize[] = {0,0};
+        varLiquidTypeInit = 131072;
+        destroyOnEmpty = 1;
+        varQuantityDestroyOnMin = 1;
+        varQuantityInit = 500;
+        varQuantityMin = 0;
+        varQuantityImax = 500;
+    };
+};
+
+class cfgLiquidDefinitions
+{
+class AgaricusLean
+    {
+     type = 131072;
+    displayName = "Lean";
+    flammability = 50;
+    class Nutrition
+	    {
+        fullnessIndex = 1;
+        energy = 75;
+        water = 200;
+        nutritionalIndex = 75;
+        toxicity = -10;
+        };
+	};
+};
